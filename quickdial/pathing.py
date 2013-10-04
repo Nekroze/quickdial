de37827d10af689907a6a294617c339f4b92c732
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-10-04 20:51:29 +1000

    added address_distance

--- quickdial/pathing.py
+++ quickdial/pathing.py
@@ -1,4 +1,5 @@
 from nekrobox.docdecs import params
+from six.moves import range
 
 
 @params(one=(int, "First symbol"),
@@ -14,4 +15,15 @@ def distance(one, two, symbols=36):
     end = symbols - highest
     loop = end + lowest
     
-    return straight if straight < loop else loop    
+    return straight if straight < loop else loop
+
+
+@params(address=(list, "Gate address as generated"),
+        symbols=(int, "Number of symbols")
+        returns=(int, "Shortest distance"))
+def address_distance(address, symbols=36):
+    """Get the shortest distance to dial this address."""
+    dist = 0
+    for pos in range(len(address[:-1])):
+        dist += distance(address[pos], address[pos+1], symbols)
+    return dist
